2043355e652dfe99251d034df4e6466577f1c187
jtibble <jtibble@gmail.com>
2014-09-09 16:42:00 -0400

    Fetching users and tournaments from service.

--- service/ui/app.js
+++ service/ui/app.js
@@ -1,20 +1,49 @@
 var app = angular.module('uiApp', []);
 
-app.controller('LandingController', function ($scope) {
-    $scope.Model = {
-        users: [
-            {
-                name: 'asdf1'
-            },
-            {
-                name: 'asdf2'
-            },
-            {
-                name: 'asdf3'
-            },
-            {
-                name: 'asdf4'
-            }
-        ]   
+app.controller('LandingController', function ($scope, FrameworkAJAX) {
+    
+    var userRequest = {
+        method: 'GET',
+        url: '/api/v0/users',
+        data: {}
     };
+    
+    $scope.Model = {};
+    
+    FrameworkAJAX.sendRequest(userRequest, function(data){
+        $scope.Model.users = data;
+    }, function(){ 
+        console.log('error getting users');
+    });
+    
+    var tournamentsRequest = {
+        method: 'GET',
+        url: '/api/v0/tournaments',
+        data: {}
+    };
+    
+    FrameworkAJAX.sendRequest(tournamentsRequest, function(data){
+        $scope.Model.tournaments = data;
+    }, function(){ 
+        console.log('error getting tournaments');
+    });
+    
+});
+
+app.provider('FrameworkAJAX', function(){
+	return {
+		$get: ['$http', function( $http ){
+			return {
+				sendRequest: function(request, successCallback, errorCallback){
+					
+					if( !request.method || !request.url || !request.data ){
+						console.log('Error making AJAX request: missing method, url, or data.');
+						return;
+					}
+					
+					return $http( request ).success( successCallback ).error( errorCallback );
+				}
+			};
+		}]		
+	};
 });
